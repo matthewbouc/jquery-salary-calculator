8d82b92587be7c2555f24b7fef14d846efc3a315
Matthew Bouc <boucgm@gmail.com>
2021-06-18 22:21:03 -0500

    Starter html with inputs, basic layout, table, button prospects

---
+++ scripts.js
@@ -0,0 +1,26 @@
+$(document).on(onReady);
+function onReady(){
+    continue;
+};
+
+
+
+
+//create submit button that collects into an object. store that object in an array.
+
+//append employee info to the DOM
+
+//from the info gathered - determine monthly costs - append to the DOM
+    // if cost exceeds $20K, signal with red element background
+
+//Create a delete button that deletes employee from the DOM.
+    // Remove employee's salary from the reported total.
+
+// Update salaries account so that the deleted employee no longer counts against cost.
+
+
+
+
+// Update the readme.md file
+
+// Extra - Add styling
